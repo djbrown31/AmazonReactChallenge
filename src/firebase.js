730d5c0eb992f7db957355aaf748531cb7400bac
Dan Brown <brown.danny.j@gmail.com>
2020-09-08 18:33:42 -0500

    react challenge commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDKue5FK67daRuRfVwhW4SXsh-jQ_WKHgQ",
+  authDomain: "clone-2cae2.firebaseapp.com",
+  databaseURL: "https://clone-2cae2.firebaseio.com",
+  projectId: "clone-2cae2",
+  storageBucket: "clone-2cae2.appspot.com",
+  messagingSenderId: "1043621233072",
+  appId: "1:1043621233072:web:b21e12e907c5633f681675",
+  measurementId: "G-K4WRD8H3KV"
+};
