21303e3047979555744c3e185fe52cede4c45d29
Dan Brown <brown.danny.j@gmail.com>
2020-09-09 15:10:08 -0500

    react challenge commit 3 day 2

--- src/reducer.js
+++ src/reducer.js
@@ -4,7 +4,7 @@ export const initialState = {
 
 // Selector
 export const getBasketTotal = (basket) =>
-    basket?.reduce((amount, item) => item.price + amount, 0)
+    basket?.reduce((amount, item) => item.price + amount, 0);
 
 const reducer = (state, action) => {
     console.log(action);
@@ -14,6 +14,24 @@ const reducer = (state, action) => {
                 ...state,
                 basket: [...state.basket, action.item],
             };
+
+        case 'REMOVE_FROM_BASKET':
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+            let newBasket = [...state.basket];
+
+            if (index >= 0) {
+                newBasket.splice(index, 1);
+            } else {
+                console.warn(`Can't remove product (id: ${action.id}) a it's not in basket!`)
+            }
+
+            return {
+                ...state,
+                basket: newBasket
+            }
+
         default:
             return state;
     }
